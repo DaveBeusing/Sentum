6859cd22e47aa072b99e97f9cb3e5703ea75604e
Dave Beusing <david.beusing@gmail.com>
2026-07-31 00:50:28 +0200

    Read scanner data from memory

--- src/sentum/scanner/SymbolScanner.hpp
+++ src/sentum/scanner/SymbolScanner.hpp
@@ -1,46 +1,21 @@
-/****
- * Copyright (C) 2025 Dave Beusing <david.beusing@gmail.com>
- * 
- * MIT License - https://opensource.org/license/mit/
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the “Software”), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is furnished 
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all 
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * 
- */
-
 #pragma once
 
 #include <string>
 #include <vector>
 
-#include <sentum/utils/Database.hpp>
-
+#include <sentum/market/MarketDataStore.hpp>
 
 struct SymbolPerformance {
-	std::string symbol;
-	double cum_return;
+    std::string symbol;
+    double cum_return;
 };
 
 class SymbolScanner {
-	public:
-		explicit SymbolScanner(Database& db, double threshold = 0.0005);
-		std::vector<SymbolPerformance> fetch_top_performers(int lookback = 60, int max_symbols = 5);
+public:
+    explicit SymbolScanner(MarketDataStore& store, double threshold = 0.0005);
+    std::vector<SymbolPerformance> fetch_top_performers(int lookback = 60, int max_symbols = 5);
 
-	private:
-		Database& database;
-		double min_return_threshold;
+private:
+    MarketDataStore& store;
+    double min_return_threshold;
 };
